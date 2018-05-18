487b4f2f459152b320be804a5d2a4d92d4ec4ed3
Артур <32384736+BulatovArthur@users.noreply.github.com>
2018-05-18 15:23:21 +0300

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -3,14 +3,10 @@
 using namespace std;
 
 void pow(int a, int b){  
-        int res = 1; 
-        if (a == 2) { 
-                cout << (a << b-1); 
-                return; 
-        } 
-        for (int i = 0; i < b; i++) 
-                res = res * a; 
-        cout << res << endl; 
+	int n = 1;
+	for (int i = 0; i < b; i++)
+                n *= a;
+	cout << "result " << n;
 }
 
 int main() {
